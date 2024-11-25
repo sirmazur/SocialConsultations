ad546455ccb9005030856111c5c6669e46ca6b98
Piotr Mazur <piotrjm02@gmail.com>
2024-11-25 01:27:42 +0100

    add comments migration

---
+++ SocialConsultations/Migrations/20241125001923_comments_update.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialConsultations.Migrations
+{
+    /// <inheritdoc />
+    public partial class comments_update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Issues_IssueId",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IssueId",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Comments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "IssueStatus",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Issues_IssueId",
+                table: "Comments",
+                column: "IssueId",
+                principalTable: "Issues",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Issues_IssueId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "IssueStatus",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IssueId",
+                table: "Comments",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Issues_IssueId",
+                table: "Comments",
+                column: "IssueId",
+                principalTable: "Issues",
+                principalColumn: "Id");
+        }
+    }
+}
